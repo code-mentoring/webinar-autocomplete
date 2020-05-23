719875324a2373990109e9980fcb7d7c763aff31
Tristan Matthias <hello@tristanmatthias.com>
2020-05-23 12:00:30 -0400

    feat: added react project

---
+++ src/Autocomplete.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Autocomplete = () => {
+  return <span>Hello</span>
+}
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {render} from 'react-dom';
+import {Autocomplete} from './Autocomplete';
+
+render(
+  <Autocomplete />,
+  document.getElementById('app')
+);
