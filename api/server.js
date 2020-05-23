27cc7b6866734960ff5b0318a1b9e50740bed08d
Tristan Matthias <hello@tristanmatthias.com>
2020-05-23 11:05:54 -0400

    feat: added search

--- api/server.js
+++ api/server.js
@@ -8,9 +8,25 @@ async function start() {
   const {data: {results: users}} = await axios('https://randomuser.me/api/?results=1000');
 
   const router = express.Router();
+
   router.get('/search', (req, res, next) => {
     const {q} = req.query;
     if (!q) return next(new Error('No query found'));
+
+    const searched = users.filter(u => {
+      const name = `${u.name.first} ${u.name.last}`.toLowerCase();
+      /**
+       * This is what the "includes" function does. (Substring search)
+       * ✅ "bob brown" -> "bob b"
+       * ✅ "bob brown" -> "BOB"
+       * ✅ "bob brown" -> "row"
+       * ❌ "bob brown" -> "BO "
+       * ❌ "bob brown" -> "bob c"
+       */
+      return name.includes(q.toLowerCase());
+    });
+
+    res.json(searched);
   });
 
   app.use(router);
