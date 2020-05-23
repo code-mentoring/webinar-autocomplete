dbb9f8b21197d99f95bb4ecbb876a3826166b64e
Tristan Matthias <hello@tristanmatthias.com>
2020-05-23 12:25:14 -0400

    feat: list users on autocomplete

--- src/Autocomplete.jsx
+++ src/Autocomplete.jsx
@@ -1,5 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 export const Autocomplete = () => {
-  return <span>Hello</span>
+  const [results, setResults] = useState(null);
+
+  const search = async q => {
+    const res = await axios(`http://localhost:9999/search?q=${q}`);
+    setResults(res.data);
+  }
+
+  return <div>
+    <input
+      type="text"
+      onChange={e => search(e.target.value)}
+    />
+    {results && <ul className="results">
+      {results.map(r => <li>
+        {r.name.first} {r.name.last}
+      </li>)}
+    </ul>}
+  </div>;
 }
